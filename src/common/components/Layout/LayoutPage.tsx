798656353690587a35be97d89aecf9a74ecbb51f
chungit201 <vuongvanchung30092001@gmail.com>
2024-05-09 21:28:24 +0700

    new design

--- src/common/components/Layout/LayoutPage.tsx
+++ src/common/components/Layout/LayoutPage.tsx
@@ -1,7 +1,7 @@
 import React, { ReactNode } from 'react'
 
-import { HeaderPage } from '@/common/components/Header'
 import { Footer } from '@/common/components/Footer'
+import { HeaderPage } from '@/common/components/Header'
 
 export const LayoutPage: React.FunctionComponent<{ children: ReactNode }> = ({ children }) => {
   return (

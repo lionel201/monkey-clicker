ddb373b3b7fb8e38f602276d810a28387cf8f776
chungit201 <vuongvanchung30092001@gmail.com>
2024-05-11 15:15:51 +0700

    responsive

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -27,3 +27,7 @@ export const formatNumberBalance = (number: number | string = '0', infractionDig
   }
   return formatPrice
 }
+
+export function ellipseAddress(address: string = '', width: number = 10): string {
+  return `${String(address).slice(0, width)}...${String(address).slice(-width)}`
+}
